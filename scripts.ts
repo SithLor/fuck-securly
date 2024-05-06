0f6b61223a8f4e8eaee13993acd143e739add3dc
SithLor <84148006+SithLor@users.noreply.github.com>
2024-05-06 10:44:46 -0700

    m

--- scripts.ts
+++ scripts.ts
@@ -1,8 +1,26 @@
-//eg input textboxe
+const DATA_URL = "data:"
 
+const data_type = {
+    text_html = "text/html",
+    text_plain = "text/plain"
+}
 
+<<<<<<< HEAD
+function gen_data_url(type,base){
+    if (base ===true ){
+=======
 const body = document.querySelector("body");
+>>>>>>> b156e3d85b5a45f3067777edba95aaba8218b27e
 
+<<<<<<< HEAD
+    }
+}
+
+//go to data:text/html,%3Ch1%3EHello%2C%20World%21%3C%2Fh1%3E
+//does not work?
+
+//https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URLs#datatexthtml3cscript3ealert2827hi27293b3c2fscript3e
+=======
 for (let index = 0; index < 1000; index++) {
     const iframe = document.createElement("iframe");
     iframe.style.display = "none";
@@ -11,3 +29,4 @@ for (let index = 0; index < 1000; index++) {
 }
 
 //site:amazonaws.com
+>>>>>>> b156e3d85b5a45f3067777edba95aaba8218b27e
